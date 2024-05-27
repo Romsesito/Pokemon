08dc83c1b54d8d5aa5d67140b468ca80a9a9efe3
Romsesito <jrom1518@gmail.com>
2024-05-27 08:46:12 -0500

    Vistas para la POKE API

--- Pokemon/ListadoPokemon.xaml.cs
+++ Pokemon/ListadoPokemon.xaml.cs
@@ -1,9 +1,30 @@
+using Newtonsoft.Json;
+using Pokemon.Models;
+using Pokemon.Services;
+using System.Diagnostics;
+
 namespace Pokemon;
 
 public partial class ListadoPokemon : ContentPage
 {
+	public List<PokemonInfo> list_pokemon;
 	public ListadoPokemon()
 	{
+		
 		InitializeComponent();
-	}
+		LoadData();
+	
+
+    }
+
+	public async void LoadData()
+    {
+        PokemonApis _poke_apis = new PokemonApis();
+        list_pokemon = await _poke_apis.DevuelveListadoPokemones();
+		Debug.WriteLine("ola");
+		Debug.WriteLine(JsonConvert.SerializeObject(list_pokemon));
+
+		listado_pokemones.ItemsSource =list_pokemon;
+    }
+
 }
